4b2a353582ebbb82cea74b6f3d6a58d2d58246b9
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-05 00:40:09 +0530

    Initial commit
    Gruntfile.js, configuring the JavaScript Task Runner.
    Help: https://github.com/gruntjs or http://gruntjs.com/

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+
+/*!
+ * node-rm-rf®
+ * The Node.js® based UNIX command for rm -rf.
+ * ____________________________________________________________________
+ * Grunt, http://gruntjs.com/
+ * The JavaScript Task Runner.
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * @mobile    : +91-8548-881059
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ____________________________________________________________________
+ * @date      : 04-Oct-2016
+ * @license   : MIT
+ * @require   : Node.js®
+ * @require   : NPM
+ * ____________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "node-rm-rf®".
+ * ____________________________________________________________________
+ */
